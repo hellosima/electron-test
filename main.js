128ca9ecf10b017941705d9e00e864937ec9b0c5
tong <tong@qq.com>
2024-07-12 16:07:18 +0800

    将应用中的chrome、node、electron版本号暴露至渲染器的预加载脚本中

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        heigth: 600
+        heigth: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     win.loadFile('index.html')
---
+++ renderer.js
@@ -0,0 +1,4 @@
+const information = document.getElementById('info')
+information.innerText = `本应用正在使用 Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), 和Electron (v${window.versions.electron()})`
+
+
